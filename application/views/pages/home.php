c58181e1f39ce22fa8f6fcc6a8bb68831ab2dd59
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-11-06 10:51:31 -0700

    Initial commit

---
+++ application/views/pages/home.php
@@ -0,0 +1,54 @@
+<!--
+Name: Natalia Ksenz
+Coding 07
+Purpose: MVC Frameworks
+
+Filename: home.php
+-->
+
+<div class="home-body">
+  <aside>
+    <p>“Bella Vita Fit&Spa” connects classic health lifestyle postulates with
+      the opening of new sport horizons. The safest high-tech equipment, a variety
+      of group programs, friendly team of professionals loving their job and
+      individual approach to each client will help you to reach your personal fit
+      goals.</p>
+    <hr>
+
+  </aside>
+
+  <section id="programs">
+    <article>
+      <img src="../../assets/images/group.jpg" alt="Group Fitness" />
+      <p>Come see what it’s like to take a high-quality fitness class taught
+        by certified personal trainers. If you have heard of it, we probably teach
+        it. From Yoga to Dance to Kickboxing to H.I.I.T. classes, we offer a wide
+        variety of disciplines.
+      </p>
+    </article>
+
+    <article>
+      <img src="../../assets/images/gym.jpg" alt="Peronal Training" />
+      <p>Personal training is a relationship, and like any other relationship,
+        both parties must invest, communicate, and make commitments to the success
+        of the partnership. Our trainers are fitness experts, life coaches and
+        nutrition guides. You can count on 100% guidance in achieving your fitness
+        goals, however, whether you meet with your trainer once or five times a
+        week, the lifestyle changes we recommend will help you achieve maximum
+        results.
+      </p>
+    </article>
+
+    <article>
+      <img src="../../assets/images/spa.jpg" alt="Spa Center" />
+      <p>Our 20, 000-square-foot Spa facility offers all of our guests a calming
+        place to refresh and relax. Spa aquazone is an escape from the everyday
+        and a journey into the world of bath from all over the globe. Experience
+        the world’s best ancient and modern therapeutic techniques in a serene setting.
+        Professional massage, face and body cosmetology, nail service will take
+        you down the path to true tranquility. All services are fully customized
+        to meet the client's needs while providing the ultimate spa experience.
+      </p>
+    </article>
+  </section>
+</div>
