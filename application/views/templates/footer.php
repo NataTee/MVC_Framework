18cbedf1de0008c0b9cfe2163adfa49f69537672
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-11-06 10:54:37 -0700

    Initial commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,15 @@
+<!--
+Name: Natalia Ksenz
+Coding 07
+Purpose: MVC Frameworks
+
+Filename: footer.php
+-->
+
+		<footer>
+				<img src="/assets/images/insta.png" alt="Instagram" />
+				<img src="/assets/images/facebook.png" alt="Instagram" />
+				<img src="/assets/images/twitter.png" alt="Instagram" />
+ 		</footer>
+	</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,30 @@
+<!--
+Name: Natalia Ksenz
+Coding 07
+Purpose: MVC Frameworks
+
+Filename: header.php
+-->
+
+<!DOCTYPE html>
+<html  lang="en-US">
+	<head>
+		<title>BellaVita Fit&Spa</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="../../assets/css/main.css" rel="stylesheet">
+		<link rel="icon" href="favicon.ico" type="image/gif">
+	</head>
+	<body>
+		<h1><?=$title ?></h1>
+		<div class="header-body">
+			<nav class="horizontalNavigation">
+				<ul>
+					<li><a href="home">Home</a></li>
+					<li><a href="about">About</a></li>
+				</ul>
+			</nav>
+
+			<h3>Fit&Spa Center in Austin, Dallas, San Antonio, Phoenix and Tempe</h3>
+			<h2>BellaVita Fit&Spa<h2>
+		</div>
