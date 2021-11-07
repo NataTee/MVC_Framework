f9bdcb5e8dcab105c3b0d023c54ad3fd2f0dde0b
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-11-06 23:41:11 -0700

    page title changed

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,14 +9,13 @@ Filename: header.php
 <!DOCTYPE html>
 <html  lang="en-US">
 	<head>
-		<title>BellaVita Fit&Spa</title>
+		<title>$title</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="../../assets/css/main.css" rel="stylesheet">
 		<link rel="icon" href="../../../favicon.ico" type="image/gif">
 	</head>
 	<body>
-		<h1><?=$title ?></h1>
 		<div class="header-body">
 			<nav class="horizontalNavigation">
 				<ul>
