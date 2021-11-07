25369cc1e4f0c49235d3d2614ecdf5abf5cc5373
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-11-06 21:56:19 -0700

    Initial commit

---
+++ application/views/pages/about.php
@@ -0,0 +1,16 @@
+<!--
+Name: Natalia Ksenz
+Coding 07
+Purpose: MVC Frameworks
+
+Filename: home.php
+-->
+
+<div class="about-body">
+  <p>
+    If you have any questions, you can find us at: <br>
+    11928 Stonehollow Dr, Austin, TX 78758<br>
+    or call us (012)345-6789.
+  </p>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1679.3368899757415!2d-97.70555322126964!3d30.40347985700749!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644cc04a13b8723%3A0x917faa48756bfdb2!2sAustin%20Community%20College%3A%20Northridge%20Campus!5e0!3m2!1sen!2sus!4v1636217978282!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
