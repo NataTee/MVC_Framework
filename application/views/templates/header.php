613fd75110a15a3d91a166f81d57be234a87e873
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-11-07 14:16:59 -0800

    title changed

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,7 +9,7 @@ Filename: header.php
 <!DOCTYPE html>
 <html  lang="en-US">
 	<head>
-		<title>$title</title>
+		<title><?=$title?></title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="../../assets/css/main.css" rel="stylesheet">
