b876e236ac600e4b043fcac4a786239b040449d8
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-11-06 22:51:38 -0700

    changed link to favicon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,7 +13,7 @@ Filename: header.php
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="../../assets/css/main.css" rel="stylesheet">
-		<link rel="icon" href="favicon.ico" type="image/gif">
+		<link rel="icon" href="../../../favicon.ico" type="image/gif">
 	</head>
 	<body>
 		<h1><?=$title ?></h1>
