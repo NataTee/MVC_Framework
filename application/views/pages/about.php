e98898bc011e1ae07e1317ee757492b3b593a280
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-11-09 13:47:49 -0800

    images moved outside of p element

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -7,62 +7,74 @@ Filename: about.php
 -->
 
 <div class="about-body">
-  <p>
-    <img src="../../assets/images/1.jpg" alt="1" />
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac sapien
-    sed lectus fringilla venenatis. Donec laoreet a nisi vehicula tempus. Nunc
-    sem metus, rhoncus ut gravida eget, commodo at odio. Fusce ut volutpat massa.
-    Vivamus consectetur, nunc eu volutpat posuere, leo neque ornare tortor, id
-    iaculis sapien enim in magna. Quisque vestibulum dui ultrices sapien commodo,
-    in condimentum felis tempor. Nullam tortor felis, pellentesque id pulvinar in,
-    convallis vitae lacus. Morbi consequat suscipit velit. Nullam scelerisque
-    tellus et sem cursus, iaculis ultrices dui venenatis. Sed euismod non sem a
-    cursus. Suspendisse volutpat, eros sit amet mollis condimentum, purus velit
-    convallis metus, id rhoncus nisl mauris in metus.
-  </p>
+  <section id="about">
+    <article>
+      <img src="../../assets/images/1.jpg" alt="1" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac sapien
+        sed lectus fringilla venenatis. Donec laoreet a nisi vehicula tempus. Nunc
+        sem metus, rhoncus ut gravida eget, commodo at odio. Fusce ut volutpat massa.
+        Vivamus consectetur, nunc eu volutpat posuere, leo neque ornare tortor, id
+        iaculis sapien enim in magna. Quisque vestibulum dui ultrices sapien commodo,
+        in condimentum felis tempor. Nullam tortor felis, pellentesque id pulvinar in,
+        convallis vitae lacus. Morbi consequat suscipit velit. Nullam scelerisque
+        tellus et sem cursus, iaculis ultrices dui venenatis. Sed euismod non sem a
+        cursus. Suspendisse volutpat, eros sit amet mollis condimentum, purus velit
+        convallis metus, id rhoncus nisl mauris in metus.
+      </p>
+    </article>
 
-  <p>
-    Quisque ut ullamcorper diam. Vivamus accumsan nulla nulla, id vestibulum orci
-    fermentum vel. Nullam vitae efficitur augue. Sed consequat leo nec sapien
-    gravida gravida. Phasellus scelerisque rhoncus tellus, facilisis sodales urna
-    ultricies id. Ut iaculis leo ac nunc ultrices, quis commodo quam blandit.
-    Duis congue urna luctus porttitor facilisis. Sed mollis tincidunt nulla quis
-    molestie. Mauris efficitur id est nec rhoncus. Vestibulum posuere laoreet
-    mauris ac placerat. Nunc ultricies feugiat aliquet. Nam commodo libero et
-    dignissim pretium. Cras sodales dolor eros, vel porttitor metus hendrerit at.
-  </p>
+    <article>
+      <p>
+        Quisque ut ullamcorper diam. Vivamus accumsan nulla nulla, id vestibulum orci
+        fermentum vel. Nullam vitae efficitur augue. Sed consequat leo nec sapien
+        gravida gravida. Phasellus scelerisque rhoncus tellus, facilisis sodales urna
+        ultricies id. Ut iaculis leo ac nunc ultrices, quis commodo quam blandit.
+        Duis congue urna luctus porttitor facilisis. Sed mollis tincidunt nulla quis
+        molestie. Mauris efficitur id est nec rhoncus. Vestibulum posuere laoreet
+        mauris ac placerat. Nunc ultricies feugiat aliquet. Nam commodo libero et
+        dignissim pretium. Cras sodales dolor eros, vel porttitor metus hendrerit at.
+      </p>
+    </article>
 
-  <p>
-    <img src="../../assets/images/2.jpg" alt="2" />
-    Maecenas aliquam vel nisi eget mattis. Cras hendrerit nunc tempus, dictum
-    massa vitae, porttitor eros. Vestibulum in quam quam. Quisque dignissim eros
-    id lorem lobortis vehicula. Aenean blandit, erat ac accumsan auctor, risus
-    risus feugiat turpis, ut vestibulum nisl est ac ligula. Mauris et luctus
-    tortor. Morbi id dignissim ligula. Quisque ut diam eu orci placerat posuere
-    ac non lacus. Maecenas nisi tortor, eleifend ut sagittis efficitur, aliquam
-    eget tellus. Etiam sed libero lectus. Etiam laoreet interdum fermentum.
-    Pellentesque dignissim ligula ac enim hendrerit, quis dignissim lorem sagittis.
-  </p>
+    <article>
+      <img src="../../assets/images/2.jpg" alt="2" />
+      <p>
+        Maecenas aliquam vel nisi eget mattis. Cras hendrerit nunc tempus, dictum
+        massa vitae, porttitor eros. Vestibulum in quam quam. Quisque dignissim eros
+        id lorem lobortis vehicula. Aenean blandit, erat ac accumsan auctor, risus
+        risus feugiat turpis, ut vestibulum nisl est ac ligula. Mauris et luctus
+        tortor. Morbi id dignissim ligula. Quisque ut diam eu orci placerat posuere
+        ac non lacus. Maecenas nisi tortor, eleifend ut sagittis efficitur, aliquam
+        eget tellus. Etiam sed libero lectus. Etiam laoreet interdum fermentum.
+        Pellentesque dignissim ligula ac enim hendrerit, quis dignissim lorem sagittis.
+      </p>
+    </article>
 
-  <p>
-    Nam semper id odio vel bibendum. Donec eu ligula blandit, condimentum ex vitae,
-    ultricies sem. Phasellus aliquet faucibus tellus eu tempus. In hac habitasse
-    platea dictumst. Morbi suscipit mi et sagittis sodales. Donec eget nibh ut
-    massa laoreet lobortis. Ut sollicitudin nisl quis mattis sodales. Curabitur
-    pellentesque urna in tempus auctor. Donec vitae dictum nisl. Nulla tincidunt
-    eget diam vel posuere. Vivamus at orci mi. Ut laoreet ligula in risus tristique
-    dapibus.
-  </p>
+    <article>
+      <p>
+        Nam semper id odio vel bibendum. Donec eu ligula blandit, condimentum ex vitae,
+        ultricies sem. Phasellus aliquet faucibus tellus eu tempus. In hac habitasse
+        platea dictumst. Morbi suscipit mi et sagittis sodales. Donec eget nibh ut
+        massa laoreet lobortis. Ut sollicitudin nisl quis mattis sodales. Curabitur
+        pellentesque urna in tempus auctor. Donec vitae dictum nisl. Nulla tincidunt
+        eget diam vel posuere. Vivamus at orci mi. Ut laoreet ligula in risus tristique
+        dapibus.
+      </p>
+    </article>
 
-  <p>
-    Praesent ut urna tincidunt, suscipit odio id, blandit lorem. Mauris gravida
-    odio eget nunc condimentum tempus. Sed enim tellus, aliquet nec mauris id,
-    ultrices viverra nulla. Pellentesque fermentum nibh eu nulla interdum
-    pellentesque at sit amet mi. Donec interdum lectus nec dui rhoncus, vitae
-    consequat nulla pulvinar. Pellentesque porta sem ac ex tincidunt hendrerit.
-    Nullam efficitur eget nunc vitae posuere. Aenean nec vestibulum dolor.
-    Pellentesque egestas, tellus aliquet tristique auctor, felis velit volutpat
-    nunc, suscipit porttitor lorem erat vel tellus. Duis eget elit a magna
-    efficitur tempor.
-  </p>
+    <article>
+      <p>
+        Praesent ut urna tincidunt, suscipit odio id, blandit lorem. Mauris gravida
+        odio eget nunc condimentum tempus. Sed enim tellus, aliquet nec mauris id,
+        ultrices viverra nulla. Pellentesque fermentum nibh eu nulla interdum
+        pellentesque at sit amet mi. Donec interdum lectus nec dui rhoncus, vitae
+        consequat nulla pulvinar. Pellentesque porta sem ac ex tincidunt hendrerit.
+        Nullam efficitur eget nunc vitae posuere. Aenean nec vestibulum dolor.
+        Pellentesque egestas, tellus aliquet tristique auctor, felis velit volutpat
+        nunc, suscipit porttitor lorem erat vel tellus. Duis eget elit a magna
+        efficitur tempor.
+      </p>
+    </article>
+  </section>
 </div>
