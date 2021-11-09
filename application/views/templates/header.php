84775f4299fbeb87721ea74292385a6fe680d6f8
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-11-08 18:33:22 -0800

    typo in the link name corrected

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -21,7 +21,7 @@ Filename: header.php
 				<ul>
 					<li><a href="home">Home</a></li>
 					<li><a href="about">About</a></li>
-					<li><a href="contacts">Contacts</a></li>
+					<li><a href="contact">Contacts</a></li>
 				</ul>
 			</nav>
 
