78d4f67b11877d3fdddf588c884de0c4eb22380f
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-11-16 12:47:00 -0800

    added link to the News page

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -22,6 +22,7 @@ Filename: header.php
 					<li><a href="home">Home</a></li>
 					<li><a href="about">About</a></li>
 					<li><a href="contact">Contacts</a></li>
+					<li><a href="news">News</a></li>
 				</ul>
 			</nav>
 
