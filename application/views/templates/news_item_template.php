7936b89fe863123d53e18bc101df3a3a779602c6
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-12-09 20:29:12 -0800

    initial commit

---
+++ application/views/templates/news_item_template.php
@@ -0,0 +1,12 @@
+<!--
+Name: Natalia Ksenz
+Final project
+Purpose: Creation of a full featured MVC website
+
+Filename: news_item_template.php
+-->
+<div>
+  <h3>{title}</h3>
+  <br>
+  {text}
+</div>
