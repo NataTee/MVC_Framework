7fb240c770eff749df2f75c98fc25c59c563e66f
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-12-09 20:26:16 -0800

    added comment and a link to Go back

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,5 +1,13 @@
+<!--
+Name: Natalia Ksenz
+Final project
+Purpose: Creation of a full featured MVC website
+
+Filename: view.php
+-->
+
 <div class="news-body">
-    <h2><?=$news_item['title']?></h2>
-    <?=$news_item['text']?>
+    <?=$news_item?>
     <br>
+    <a href="/news">Go back...</a>
 </div>
