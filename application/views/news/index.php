3e9472536b2142e077d4aa1cc8b643a7eaaadf87
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-12-09 19:57:19 -0800

    modified to eliminate division of concerns violations

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,12 +1,5 @@
-<h2><?=$title?></h2>
-
-<?php foreach ($news as $news_item): ?>
-
-        <h3><?=$news_item['title']?></h3>
-        <div class="main-news">
-                <?=$news_item['text']?>
-                <br>
-                <p><a href="<?='/news/'.$news_item['slug']?>">Read more...</a></p>
-        </div>
-        
-<?php endforeach; ?>
+<div class="main-news">
+    <h2>News Archive</h2>
+    <br>
+    <p><?=$news?></p>
+</div>
