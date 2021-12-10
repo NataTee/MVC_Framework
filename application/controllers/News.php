c89169500698af1acc15ac314cbfe9d988cdc890
Natalia Ksenz <velvet@Natalias-MacBook-Pro.local>
2021-12-09 19:50:27 -0800

    added parser library links

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -8,25 +8,30 @@ class News extends CI_Controller {
     }
 
     public function index() {
-        $data['news'] = $this->news_model->get_news();
+        // get the stories by from the model
         $data['title'] = 'News archive';
+        $news['news'] = $this->news_model->get_news();
+
+        $data['news'] = $this->parser->parse('templates/news_template', $news, TRUE);
 
         $this->load->view('templates/header', $data);
-        $this->load->view('news/index', $data);
+        $this->load->view('news/index');
         $this->load->view('templates/footer');
     }
 
     public function view($slug = NULL) {
-        $data['news_item'] = $this->news_model->get_news($slug);
+        // get the stories by from the model
+        $data['title'] = 'News archive';
+        $news_item = $this->news_model->get_news($slug);
 
         if (empty($data['news_item'])) {
             show_404();
         }
 
-        $data['title'] = $data['news_item']['title'];
+        $data['news_item'] = $this->parser->parse('templates/news_item_template', $news_item, TRUE);
 
         $this->load->view('templates/header', $data);
-        $this->load->view('news/view', $data);
+        $this->load->view('news/view');
         $this->load->view('templates/footer');
     }
 }
